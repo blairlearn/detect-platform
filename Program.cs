dec6b372b9a5048adb3140b9bda76c03ff3d0408
Blair Learn <blairlearn@users.noreply.github.com>
2023-05-22 16:49:38 -0400

    Detect the OS.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace detect_platform;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"This is Windows: {RuntimeInformation.IsOSPlatform(OSPlatform.Windows)}");
+        Console.WriteLine($"  This is Linux: {RuntimeInformation.IsOSPlatform(OSPlatform.Linux)}");
+        Console.WriteLine($"  This is MacOS: {RuntimeInformation.IsOSPlatform(OSPlatform.OSX)}");
+        Console.WriteLine($"This is FreeBSD: {RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD)}");
+    }
+}
